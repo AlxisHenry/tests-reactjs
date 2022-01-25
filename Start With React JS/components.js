710e3ad02015625cf7d0b009a359c29aab2cf8d5
AlxisHenry <alexis.henry150357@gmail.com>
2022-01-25 14:44:32 +0100

    Add a pause & reset button for the timer : create functions in timer component

--- Start With React JS/components.js
+++ Start With React JS/components.js
@@ -1,76 +1,144 @@
-function WelcomeFunc({name, children}) {
-    return <div>
-        <h1> Bonjour  {name} </h1>
-        <p>
-            {children}
-        </p>
+function WelcomeFunc({ name, children }) {
+  return (
+    <div>
+      <h1> Bonjour {name} </h1>
+      <p>{children}</p>
     </div>
+  );
 }
 
 class Welcome extends React.Component {
-    render() {
-        return <div>
-        <h1> Bonjour  {this.props.name} </h1>
-        <p>
-            {this.props.children}
-        </p>
-    </div>
-    }
+  render() {
+    return (
+      <div>
+        <h1> Bonjour {this.props.name} </h1>
+        <p>{this.props.children}</p>
+      </div>
+    );
+  }
 }
-class Clock extends React.Component { // Date dynamique 
-    constructor (props) {
-        super(props) 
-        this.state = {date: new Date}
-        this.timer = null
-    }
-    componentDidMount () {
-        this.timer = window.setInterval(this.tick.bind(this), 1000)
-    }
-    componentwillUnmout () {
-        window.clearInterval(this.timer)
-    }
-    tick () {
-        this.setState({date: new Date()})
-    }
-    render() {
-        const date = new Date()
-        return <div> Il est {this.state.date.toLocaleDateString()} {this.state.date.toLocaleTimeString( )}</div>
-    }
+class Clock extends React.Component {
+  // Date dynamique
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+    this.timer = null;
+  }
+  componentDidMount() {
+    this.timer = window.setInterval(this.tick.bind(this), 1000);
+  }
+  componentwillUnmout() {
+    window.clearInterval(this.timer);
+  }
+  tick() {
+    this.setState({ date: new Date() });
+  }
+  render() {
+    const date = new Date();
+    return (
+      <div>
+        {" "}
+        Il est {this.state.date.toLocaleDateString()}{" "}
+        {this.state.date.toLocaleTimeString()}
+      </div>
+    );
+  }
 }
 
 class Incrementer extends React.Component {
-    constructor (props) {
-        super(props)
-        this.state = {n: props.start}
-        this.timer = null
-    }
-    componentDidMount() {
-        window.setInterval(this.increment.bind(this), 400)
-    }
-    componentwillUnmout() {
-        window.clearInterval(this.timer)
-    }
-    increment() {
-        // this.setState({n: this.state.n + 1}) 
-        this.setState(function (state, props) {
-            return {n: state.n + 9}
-        })
-    }
-    render() {
-        return <div> {this.state.n} </div>
-    }   
+  constructor(props) {
+    super(props);
+    this.state = { n: props.start, timer: null };
+    this.toggle = this.toggle.bind(this)
+    this.reset = this.reset.bind(this)
+  }
+  componentDidMount() {
+    this.play();
+  }
+  componentwillUnmout() {
+    window.clearInterval(this.state.timer);
+  }
+  increment() {
+    // this.setState({n: this.state.n + 1})
+    this.setState((state, props) => ({ n: state.n + props.step }));
+  }
+  pause() {
+    window.clearInterval(this.state.timer);
+    this.setState({ timer: null})
+  }
+  play() {
+    window.clearInterval(this.state.timer);
+    this.setState({
+        timer: window.setInterval(this.increment.bind(this), 400) 
+    })
+}
+toggle() {
+    return this.state.timer ? this.pause() : this.play()
+}
+ label() {
+     return this.state.timer ? 'Pause' : 'Lecture'
+ }
+ reset() {
+    this.pause()
+    this.setState((state, props) => ({ n: 0 }));
+    this.play()
+}
+  render() {
+    const method = this.state.timer ? this.pause : this.play
+    const label = this.state.timer ? this.pause : this.play
+    return (
+      <div>
+        Valeur: {this.state.n} <br></br>
+            {/* {this.state.timer ? 
+                    <button onClick={this.pause.bind(this)}>Pause</button> :
+                    <button onClick={this.play.bind(this)}>Play</button>} */}
+            <button onClick={this.toggle}>
+                    {this.label()}
+            </button>           
+            <button onClick={this.reset}>Reset
+            </button>
+      </div>
+    );
+  }
 }
 
-function Home ()
- {
-     return <div>
-         <Welcome name="Dorothée"/>
-         <Welcome name="Jean"/>
-         <Clock/>
-         <Incrementer start={10}/>
-     </div>
- }
+Incrementer.defaultProps = {
+  start: 0,
+  step: 1,
+};
 
+class ManualIncrementer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { n: 0 };
+  }
+  increment(e) {
+    console.log(e);
+    // this.setState({n: this.state.n + 1})
+    this.setState((state, props) => ({ n: state.n + 1 }));
+  }
+  render() {
+    return (
+      <div>
+        {" "}
+        Valeur : {this.state.n}{" "}
+        <button onClick={this.increment.bind(this)}>Incrémenter</button>
+      </div>
+    );
+  }
+}
 
+function Home() {
+  return (
+    <div>
+      {/* <Welcome name="Dorothée"/> */}
+      <Welcome name="Jean" />
+      <Clock />
+      <Incrementer start={10} />
+      {/* <Incrementer start={20} step={10}/> */}
+      {/* <ManualIncrementer /> */}
+    </div>
+  );
+}
 
-ReactDOM.render(<Home/>, document.querySelector('#app'))        
+ReactDOM.render(<Home />, document.querySelector("#app"));
